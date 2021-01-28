f65c69d08280849651f469fd101c322cf18e4b1e
lenconda <i@lenconda.top>
2021-01-28 11:15:18 +0800

    feat: announce v0.4.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { DollieGenerator } from '@dollie/core';
+import { DollieInteractiveGenerator } from '@dollie/core';
 import Environment from 'yeoman-environment';
 
 const env = Environment.createEnv();
-env.registerStub(DollieGenerator, 'dollie');
+env.registerStub(DollieInteractiveGenerator, 'dollie');
 env.run('dollie', null);
