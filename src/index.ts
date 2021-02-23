4e74479f8702f2e37a9e6d87e71a1ac7676d0c66
lenconda <i@lenconda.top>
2021-02-23 19:28:29 +0800

    feat: update API from @dollie/core

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,53 @@
 import path from 'path';
-import fs from 'fs';
-import Environment from 'yeoman-environment';
+import fs from 'fs-extra';
 import { program } from 'commander';
-import {
-  DollieInteractiveGenerator,
-  DollieComposeGenerator,
-  readJson,
-  parseComposeConfig,
-} from '@dollie/core';
+import dollie, { parseComposeConfig, log } from '@dollie/core';
+import constantsConfig from '@dollie/core/lib/constants';
+import { ExportedConstants } from '@dollie/core/lib/interfaces';
+import { parseCamelToSnake, parseSnakeToKebab } from './utils/parsers';
 
-const packageJson = readJson(path.resolve(__dirname, '../package.json'));
-
-const env = Environment.createEnv();
-env.registerStub(DollieInteractiveGenerator, 'dollie:interactive');
-env.registerStub(DollieComposeGenerator, 'dollie:compose');
+const packageJson = fs.readJSONSync(path.resolve(__dirname, '../package.json'));
 
 program.version(packageJson.version || '0.0.1');
 
-program.action(() => {
-  env.run('dollie:interactive', null);
+const constantKeys = Object.keys(constantsConfig) as Array<keyof ExportedConstants>;
+
+for (const constantKey of constantKeys) {
+  const kebabKey = parseSnakeToKebab(constantKey);
+  if (kebabKey) {
+    if (constantKey === 'SCAFFOLD_TIMEOUT') {
+      program.option(`--${kebabKey} <number> [value]`);
+      continue;
+    }
+    program.option(`--${kebabKey} <text> [value]`);
+  }
+}
+
+program.action(async (...props) => {
+  const constants = Object.keys(props[0] || {}).reduce((result, currentKey) => {
+    const currentSnakeKey = parseCamelToSnake(currentKey);
+    const currentValue = (props[0] || {})[currentKey];
+    if (currentSnakeKey && currentValue !== undefined) {
+      const snakeKey = currentSnakeKey.toUpperCase() as keyof ExportedConstants;
+      if (snakeKey === 'SCAFFOLD_TIMEOUT') {
+        result[snakeKey] = parseInt(currentValue, 10) || 10000;
+      } else {
+        result[snakeKey] = currentValue;
+      }
+    }
+    return result;
+  }, {}) as ExportedConstants;
+  try {
+    await dollie.interactive({
+      constants,
+    });
+  } catch (e) {
+    log.error(e.toString());
+    process.exit(1);
+  }
 });
 
-program.command('compose <config>').action((config: string) => {
+program.command('compose <config>').action(async (config: string) => {
   const configFilePath = path.join(process.cwd(), config);
 
   if (!fs.existsSync(configFilePath)) {
@@ -35,7 +61,13 @@ program.command('compose <config>').action((config: string) => {
 
   const content = fs.readFileSync(configFilePath, { encoding: 'utf-8' });
   const composeConfig = parseComposeConfig(content);
-  env.run('dollie:compose', composeConfig, null);
+
+  try {
+    await dollie.compose(composeConfig);
+  } catch (e) {
+    log.error(e);
+    process.exit(1);
+  }
 });
 
 program.parse(process.argv);
